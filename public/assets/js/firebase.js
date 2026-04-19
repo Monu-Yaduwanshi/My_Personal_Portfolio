03c4baf9854a9d3c14c701f0b5e20d20e16051c7
Monu Yaduwanshi <154736453+Monu-Yaduwanshi@users.noreply.github.com>
2026-04-19 17:35:49 +0530

    Update firebase.js

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpNkgsy00R55552kBEWklmdMIBvAaTMyOvxWc",
-  authDomain: "monuyaduwanshiportfolio.firebaseapp.com",
-  projectId: "monuyadu5wanshiportfolio",
+  apiKey: "AIzaSy277DpNkgsy00R555752kBEWklmdMIBvAaTMyOvxWc",
+  authDomain: "monuyaduwa272nshiportfolio.firebaseapp.com",
+  projectId: "monuyadu5wa2nshiportfolio",
   storageBucket: "monuyaduw5anshiportfolio.fireba5sestorage.app",
-  messagingSenderId: "1045228868439989",
+  messagingSenderId: "104522886278439989",
   appId: "1:1045268439989:web:32fdfd88de555b5337709affc",
-  measurementId: "G-L37882MCEVBL6"
+  measurementId: "G-L3782782MCEVBL6"
 };
 
 const app = initializeApp(firebaseConfig);
